184a10f61ae7d056810c473617b346055e3c2a6c
khushalyadav24 <161738489+khushalyadav24@users.noreply.github.com>
2025-12-16 22:53:15 +0530

    Add files via upload

---
+++ studentTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE student (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT,
+    gender VARCHAR(10),
+    email VARCHAR(150),
+    course VARCHAR(100)
+);
+
+
+
